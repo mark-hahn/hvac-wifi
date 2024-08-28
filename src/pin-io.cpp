9fd4e1188085d4d89dc8eb62420ce6764300b33d
Mark Hahn <mark@hahnca.com>
2024-08-28 14:49:58 -0700

    pin-io var renames

--- src/pin-io.cpp
+++ src/pin-io.cpp
@@ -6,6 +6,7 @@
 #include "pins.h"
 #include "wifi-sta.h"
 
+#define DEBOUNCE_DELAY_MS 2
 #define Y_DELAY_MS 5000
 
 u8 ledInPins[] = {
@@ -32,14 +33,6 @@ u8 ledOutPins[sizeof ledInPins] = {
   PIN_LED_PWR
 };
 
-void readLedPins() {
-
-}
-
-void writeLedPins() {
-
-}
-
 const char* pinToName(u8 pin) {
   switch(pin){
     case PIN_IN_Y1 : return (const char *) "Y1" ;
@@ -54,23 +47,26 @@ const char* pinToName(u8 pin) {
   }
 }
 
-u8  inPinLvls[sizeof ledInPins]  = {255};
-u8 outPinLvls[sizeof ledOutPins] = {255};
+u8   inPinLvls[sizeof ledInPins]   = {255};
+u8   outPinLvls[sizeof ledOutPins] = {255};
+bool pinChanged[sizeof ledInPins]  = {false};
 
 // send pin status to server when any pin changes
-void sendPinStatus() {
+void sendChangedPins() {
   char json[128];
   json[0] = '{';
   json[1] = 0;
   for (int pinIdx = 0; pinIdx < (sizeof ledOutPins); pinIdx++) {
-    const char *name = pinToName(pinIdx);
-    u8 pinLvl = outPinLvls[pinIdx];
-    char msg[16];
-    if (pinLvl) sprintf(msg, "\"%s\":true,",   name);
-    else        sprintf(msg, "\"%s\":false,",  name);
-    strcat(json, msg);
+    if(pinChanged[pinIdx]) {
+      const char* name = pinToName(pinIdx);
+      u8 pinLvl        = outPinLvls[pinIdx];
+      char msg[32];
+      if (pinLvl) sprintf(msg, "\"%s\":true,",  name);
+      else        sprintf(msg, "\"%s\":false,", name);
+      strcat(json, msg);
+    }
   }
-  json[strlen(json) - 1] = '}';
+  json[strlen(json)-1] = '}';
   wsSend(json);
 }
 
@@ -89,27 +85,27 @@ void pinIoSetup() {
   pinMode(PIN_ENBL_WIFI,   INPUT);
   pinMode(PIN_OPEN_Y1,     OUTPUT);  
   pinMode(PIN_OPEN_Y2,     OUTPUT);  
-  attachInterrupt(
-    PIN_IN_PWR, handlePowerPinFall, FALLING);  
   digitalWrite(PIN_OPEN_Y1, LOW);
   digitalWrite(PIN_OPEN_Y2, LOW);
+  attachInterrupt(
+    PIN_IN_PWR, handlePowerPinFall, FALLING);  
 }
 
 void pinIoLoop() {
-  static u32  fanOnTime                   = 0;
-  static bool waitingForFanDelay          = false;
+  static u32  fanOnTime        = 0;
+  static bool waitingForYDelay = false;
 
   u32 now = millis();
 
   if(lastPwrFallMs) {
-    if((now - lastPwrFallMs) > 2) {
+    if((now - lastPwrFallMs) > DEBOUNCE_DELAY_MS) {
       lastPwrFallMs = 0;
-      // power fall debounced
 
+      // power fall debounced, arm interrupt for next fall
       attachInterrupt(
         PIN_IN_PWR, handlePowerPinFall, FALLING); 
 
-      // middle of power pulse, all pin inputs valid
+      // inside power pulse, all pin inputs valid
       bool havePinChg    = false;
       bool haveFanPinChg = false;
       for(int pinIdx = 0; 
@@ -119,20 +115,24 @@ void pinIoLoop() {
         inPinLvls[pinIdx] = inPinLvl;
         if (inPinLvl == outPinLvls[pinIdx]) {
           // input pin changed
-          havePinChg = true;
-          if(pinIdx == FAN_PIN_IDX) haveFanPinChg = true;
+          pinChanged[pinIdx] = true;
+          havePinChg         = true;
+          if(pinIdx == FAN_PIN_IDX) 
+               haveFanPinChg = true;
           int outPinGpioNum  = ledOutPins[pinIdx];
-          u8 outPinLvl       = !inPinLvl;
+          u8  outPinLvl      = !inPinLvl;
           outPinLvls[pinIdx] = outPinLvl;
           digitalWrite(outPinGpioNum, outPinLvl);
         }
+        else pinChanged[pinIdx] = false;
       }
-      if(havePinChg) sendPinStatus();
+      if(havePinChg) sendChangedPins();
+
       if(haveFanPinChg) {
         if(!inPinLvls[FAN_PIN_IDX]) {
           // fan turned on -> Y relay stays open
-          fanOnTime          = now;
-          waitingForFanDelay = true;
+          fanOnTime        = now;
+          waitingForYDelay = true;
         } else {  
           // fan turned off -> open Y relay 
           digitalWrite(PIN_OPEN_Y1, HIGH);
@@ -141,10 +141,10 @@ void pinIoLoop() {
       }
     }
   }
-  if(waitingForFanDelay && 
+  if(waitingForYDelay && 
       (now - fanOnTime) > Y_DELAY_MS) {
-    // fan delay timeout -> close Y relay 
-    waitingForFanDelay = false;
+    // Y delay timeout -> close Y relay 
+    waitingForYDelay = false;
     digitalWrite(PIN_OPEN_Y1, LOW);
     digitalWrite(PIN_OPEN_Y2, LOW);
   }
