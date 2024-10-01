991a590fae76cda170af3a21a030f4685b553ec5
Mark Hahn <mark@hahnca.com>
2024-10-01 14:01:18 -0700

    set fixed ip in wifi-sta

--- src/wifi-sta.h
+++ src/wifi-sta.h
@@ -1,6 +1,10 @@
 #ifndef WIFI_H
 #define WIFI_H
 
+// FIXED IP IS DEFINED IN ROUTER
+// MAC is c8:2e:18:f0:06:2c
+// IP  is 192.168.1.106
+
 #define DEF_SSID     "hahn-fi"
 #define DEF_PASSWORD "90-NBVcvbasd"
 
